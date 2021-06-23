4fb1ebbe4162e64d18492954f6669dad2e30f36d
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-06-23 08:40:51 -0500

    Question 7 & 8

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -48,10 +48,29 @@ for (let i = 0; i < supplyChanges.length; i++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
+for (supply of supplyChanges){
+  if (supply > 0){
+    console.log(`Added ${supply} parts`);
+  } else if (supply === 0) {
+    console.log('No Change.');
+  } else {
+    console.log(`Removed ${supply} parts`);
+  }
+}
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
-
+let index = 0;
+while (index < supplyChanges.length){
+  if (supplyChanges[index] > 0){
+    console.log(`Added ${supplyChanges[index]} parts`);
+  } else if (supplyChanges[index] === 0) {
+    console.log('No Change.');
+  } else {
+    console.log(`Removed ${supplyChanges[index]} parts`);
+  }
+  index++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
